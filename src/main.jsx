51ea46a6d9ac8372359f8471840bca3222aad2f4
Suman-1997 <ny619619@gmail.com>
2024-09-17 12:16:17 +0530

    Created React Element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Click on me to go google</a>
+)
+// const reactElement = React.createElement(
+//   'a',
+//   {
+//     href : "https://google.com",
+//     target : _blank
+//   },
+//   "Click on me Bankai"
+// )
+createRoot(document.getElementById('root')).render(  
+  // anotherElement
+  <App />
 )
